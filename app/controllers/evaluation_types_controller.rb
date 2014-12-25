146c5378b2480581f04435ce456c8055fd8e611f
Adrian <adrian.chang.alcover@gmail.com>
2014-12-24 23:43:46 -0500

    # Never trust parameters from the scary internet, only allow the white list through.     def evaluation_type_params       params.require(:evaluation_type).permit(:short_name, :full_name)     end

--- app/controllers/evaluation_types_controller.rb
+++ app/controllers/evaluation_types_controller.rb
@@ -69,6 +69,6 @@ class EvaluationTypesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def evaluation_type_params
-      params.require(:evaluation_type).permit(:name)
+      params.require(:evaluation_type).permit(:short_name, :full_name)
     end
 end
