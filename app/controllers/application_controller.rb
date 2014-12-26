de75158ccd9b2e497cb6d2587490fa4bcdd71154
Adrian <adrian.chang.alcover@gmail.com>
2014-12-26 13:45:40 -0500

    defining constants for CurriculumType and EvaluationType

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  CURRICULUM_TYPE_BASICO = CurriculumType.first
+  CURRICULUM_TYPE_PROPIO = CurriculumType.second
+  CURRICULUM_TYPE_OPTATIVO = CurriculumType.third
+
+  EVALUATION_TYPE_EXAMEN_FINAL = EvaluationType.first
+  EVALUATION_TYPE_TRABAJO_CURSO = EvaluationType.second
 end
