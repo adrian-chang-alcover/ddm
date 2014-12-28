cd379c237843acbcfda3b0acbaf95fc70798014f
Adrian <adrian.chang.alcover@gmail.com>
2014-12-28 09:25:22 -0500

    rails generate bootstrap:themed StudyPlans

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -1,2 +1,3 @@
 class StudyPlan < ActiveRecord::Base
+  has_many :years
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,4 +1,5 @@
 class Year < ActiveRecord::Base
   belongs_to :career
+  belongs_to :study_plan
   has_many :semesters, dependent: :destroy
 end
