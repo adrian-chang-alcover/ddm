527faebc15ed3c86b8d41704a89cb4885c34d6cf
Adrian <adrian.chang.alcover@gmail.com>
2014-12-28 09:30:20 -0500

    fixing bug

--- db/migrate/20141228141422_create_study_plan.rb
+++ db/migrate/20141228141422_create_study_plan.rb
@@ -1,7 +1,13 @@
 class CreateStudyPlan < ActiveRecord::Migration
-  def change
+  def up
     create_table :study_plans do |t|
       t.string :name
+
+      t.timestamps
     end
+  end
+
+  def down
+    drop_table :study_plans
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,9 @@ ActiveRecord::Schema.define(version: 20141228141505) do
   end
 
   create_table "study_plans", force: true do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "subjects", force: true do |t|
