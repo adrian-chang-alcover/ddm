f19bdc10e773eee77e6815e27358f7d7f6632603
Adrian <adrian.chang.alcover@gmail.com>
2014-12-28 11:10:37 -0500

    $ (   -> $('select#career option').click(     -> document.location.search = "?career=" + $(this).val()   ) )
    $ (
      -> $('select#career').val($('#career_id').val())
    )

--- app/controllers/ppd_controller.rb
+++ app/controllers/ppd_controller.rb
@@ -1,4 +1,9 @@
 class PpdController < ApplicationController
   def report
+    if params[:career]
+      @career = Career.find(params[:career])
+    else
+      @career = Career.first
+    end
   end
 end
