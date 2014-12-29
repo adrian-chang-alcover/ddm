b04ee3ba35820a775023a512a698c60d0c6e471e
Adrian <adrian.chang.alcover@gmail.com>
2014-12-29 16:16:14 -0500

    programing _malla.html.erb

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,4 +1,22 @@
 class Semester < ActiveRecord::Base
   belongs_to :year
   has_many :subjects, dependent: :destroy
+
+  default_scope {order :name}
+
+  def pretty_name
+    case name
+      when '1'
+        'primer semestre'
+      when '2'
+        'segundo semestre'
+      else
+        name
+    end
+  end
+
+  def weeks
+    return 16
+    self.end_date.cweek - self.begin_date.cweek
+  end
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -4,4 +4,23 @@ class Year < ActiveRecord::Base
   has_many :semesters, dependent: :destroy
 
   default_scope {order :name}
+
+  def pretty_name
+    case name
+      when '1'
+        'primer año'
+      when '2'
+        'segundo año'
+      when '3'
+        'tercer año'
+      when '4'
+        'cuarto año'
+      when '5'
+        'quinto año'
+      when '6'
+        'sexto año'
+      else
+        name
+    end
+  end
 end
