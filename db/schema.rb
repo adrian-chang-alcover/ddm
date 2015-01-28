4f209f8fa02d7f741d0ea241f542a41647c18baa
Adrian <adrian.chang.alcover@gmail.com>
2015-01-28 16:25:14 -0500

    rails generate public_activity:migration rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128200101) do
+ActiveRecord::Schema.define(version: 20150128212430) do
+
+  create_table "activities", force: true do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
 
   create_table "careers", force: true do |t|
     t.string   "short_name"
@@ -71,7 +88,7 @@ ActiveRecord::Schema.define(version: 20150128200101) do
     t.string   "full_name"
     t.integer  "discipline_id"
     t.integer  "semester_id"
-    t.integer  "class_hours",            default: 0
+    t.integer  "class_hours_1",          default: 0
     t.integer  "evaluation_type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -79,7 +96,35 @@ ActiveRecord::Schema.define(version: 20150128200101) do
     t.integer  "curriculum_type_id"
     t.string   "dictum_or_resoluteness"
     t.integer  "number",                 default: 0
+    t.integer  "class_hours_2",          default: 0
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
   create_table "years", force: true do |t|
     t.string   "name"
