c69f481fa4c0d4ac4e6f4e861a41be606b2cd4d6
Adrian <adrian.chang.alcover@gmail.com>
2015-01-28 15:13:59 -0500

    migrating 20150128200101_dividing_class_hours_in_annual_subjects.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128190910) do
+ActiveRecord::Schema.define(version: 20150128200101) do
 
   create_table "careers", force: true do |t|
     t.string   "short_name"
