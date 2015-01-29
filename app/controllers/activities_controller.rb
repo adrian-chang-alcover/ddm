481cbb21387d4234b0afb3ef8aa410d0d0432f01
Adrian <adrian.chang.alcover@gmail.com>
2015-01-29 13:36:46 -0500

    activities_controller.rb logic. DONE

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,9 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.where(trackable_type: 'Subject')
+    unless params['user'].blank?
+      @activities = PublicActivity::Activity.where(trackable_type: params['entity'], owner_type: 'User', owner_id: params['user'])
+    else
+      @activities = PublicActivity::Activity.where(trackable_type: params['entity'])
+    end
   end
 end
