5eb659051564244561c0f5e258aa33e81b017599
Adrian <adrian.chang.alcover@gmail.com>
2015-01-28 22:58:27 -0500

    listing activities on subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -61,6 +61,11 @@ class SubjectsController < ApplicationController
     end
   end
 
+  def activities
+    @activities = PublicActivity::Activity.where(trackable_type: "Subject")
+    render 'public_activity/index'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_subject
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get 'subjects/activities'
+
   get 'ppd/report'
   get 'ppd/export'
 
