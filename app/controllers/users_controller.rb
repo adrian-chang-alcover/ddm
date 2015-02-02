6b54c75ca16f5e00f29034a25b4f54c520d9fdc3
Adrian <adrian.chang.alcover@gmail.com>
2015-02-02 14:24:41 -0500

    implementing users/ index.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all if current_user.admin? or current_user.ddm?
+    @users = current_user.faculty.users if current_user.decano? or current_user.vicedecano?
   end
 
   def roles
