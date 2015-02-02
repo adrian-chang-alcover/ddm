7525bcd67f8cfe85ac45f16941112fdece4fc783
Adrian <adrian.chang.alcover@gmail.com>
2015-02-02 14:07:01 -0500

    migrating 20150202190537_add_priority_to_roles.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202174741) do
+ActiveRecord::Schema.define(version: 20150202190537) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20150202174741) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "priority"
   end
 
   create_table "roles_users", force: true do |t|
