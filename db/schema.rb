f8b005809d22564738d17250b866ecf92f8d12f4
Adrian <adrian.chang.alcover@gmail.com>
2015-02-02 11:11:35 -0500

    rails generate model Role name:string and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131162135) do
+ActiveRecord::Schema.define(version: 20150202160841) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(version: 20150131162135) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "dean"
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "semesters", force: true do |t|
