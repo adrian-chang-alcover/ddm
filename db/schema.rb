e0cbdbc10ff39d1f35007c137892a1da1bbab6af
Adrian <adrian.chang.alcover@gmail.com>
2015-02-02 11:15:16 -0500

    migrating 20150202161306_create_roles_users.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202160841) do
+ActiveRecord::Schema.define(version: 20150202161306) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -69,6 +69,13 @@ ActiveRecord::Schema.define(version: 20150202160841) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", force: true do |t|
+    t.integer  "role_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
