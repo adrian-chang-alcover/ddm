3927b5c2fd095a85eba224c57c6e14275887b2a8
Adrian <adrian.chang.alcover@gmail.com>
2015-03-26 16:01:26 -0400

    running migration to add career_id column in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326183434) do
+ActiveRecord::Schema.define(version: 20150326195346) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 20150326183434) do
     t.datetime "updated_at"
     t.string   "username"
     t.integer  "faculty_id"
+    t.integer  "career_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
