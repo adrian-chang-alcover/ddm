32684a83f7e494e1ae1eed576ec721bd2e78027c
Adrian <adrian.chang.alcover@gmail.com>
2015-03-25 22:30:58 -0400

    migrating 20150326022905_adding_study_modality_column_to_career_table.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326021446) do
+ActiveRecord::Schema.define(version: 20150326022905) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150326021446) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "faculty_id"
+    t.integer  "study_modality_id"
   end
 
   create_table "curriculum_types", force: true do |t|
