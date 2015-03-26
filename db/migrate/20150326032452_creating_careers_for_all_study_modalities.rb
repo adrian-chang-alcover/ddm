aaac8f1a02a561126846ba0e42bc4bb751584751
Adrian <adrian.chang.alcover@gmail.com>
2015-03-26 09:36:03 -0400

    migrating

--- db/migrate/20150326032452_creating_careers_for_all_study_modalities.rb
+++ db/migrate/20150326032452_creating_careers_for_all_study_modalities.rb
@@ -2,23 +2,31 @@ class CreatingCareersForAllStudyModalities < ActiveRecord::Migration
   def up
     crd = StudyModality.find_by_short_name('CRD')
     StudyModality.where("id != ?", crd.id).each do |modality|
+      puts modality.full_name
+      
       crd.careers.each do |c|
         career = Career.find_or_create_by(full_name: c.full_name, faculty: c.faculty, study_modality: modality)
+        puts "\t#{career.name}"  
 
         # cloning years and its semesters
         c.years.each do |y|
           year = Year.find_or_create_by(name: y.name, career: career, study_plan: y.study_plan)
+          puts "\t\t#{year.pretty_name}"
+
           y.semesters.each do |s|
             Semester.find_or_create_by(name: s.name, begin_date: s.begin_date, end_date: s.end_date, weeks: s.weeks, year: year)
+            puts "\t\t\t#{s.pretty_name}"
           end
         end
 
         # cloning disciplines
         c.disciplines.each do |d|
           Discipline.find_or_create_by(name: d.name, career: career, number: d.number)
+          puts "\t\t#{d.name}"
         end
 
         # cloning subjects
+        puts "Cloning #{c.subjects.count} subjects"
         c.subjects.each do |s|
           year = Year.find_by(name: s.year.name, career: career, study_plan: s.year.study_plan)
           semester = Semester.find_by(name: s.semester.name, year: year)
