94d728c91c3086a3a4efa35fc9f927da56b7c9c8
Adrian <adrian.chang.alcover@gmail.com>
2015-03-29 13:09:58 -0400

    replacing descipline_id by discipline_id

--- test/controllers/subjects_controller_test.rb
+++ test/controllers/subjects_controller_test.rb
@@ -18,7 +18,7 @@ class SubjectsControllerTest < ActionController::TestCase
 
   test "should create subject" do
     assert_difference('Subject.count') do
-      post :create, subject: { descipline_id: @subject.descipline_id, evaluation_type_id: @subject.evaluation_type_id, full_name: @subject.full_name, hours: @subject.hours, semester_id: @subject.semester_id, short_name: @subject.short_name }
+      post :create, subject: { discipline_id: @subject.discipline_id, evaluation_type_id: @subject.evaluation_type_id, full_name: @subject.full_name, hours: @subject.hours, semester_id: @subject.semester_id, short_name: @subject.short_name }
     end
 
     assert_redirected_to subject_path(assigns(:subject))
@@ -35,7 +35,7 @@ class SubjectsControllerTest < ActionController::TestCase
   end
 
   test "should update subject" do
-    patch :update, id: @subject, subject: { descipline_id: @subject.descipline_id, evaluation_type_id: @subject.evaluation_type_id, full_name: @subject.full_name, hours: @subject.hours, semester_id: @subject.semester_id, short_name: @subject.short_name }
+    patch :update, id: @subject, subject: { discipline_id: @subject.discipline_id, evaluation_type_id: @subject.evaluation_type_id, full_name: @subject.full_name, hours: @subject.hours, semester_id: @subject.semester_id, short_name: @subject.short_name }
     assert_redirected_to subject_path(assigns(:subject))
   end
 
