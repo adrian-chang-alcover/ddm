bcd98deb192a099a0cf0e5ac015b5e16dfcdd618
Adrian <adrian.chang.alcover@gmail.com>
2015-04-03 23:41:10 -0400

    rails generate scaffold University short_name:string full_name:string and migrating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404032655) do
+ActiveRecord::Schema.define(version: 20150404033030) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
