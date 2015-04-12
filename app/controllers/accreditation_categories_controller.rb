c90d11cd2cd5b9adefc455b9d25a497b7357dec0
Adrian <adrian.chang.alcover@gmail.com>
2015-04-12 10:49:38 -0400

    rails g bootstrap:themed AccreditationCategories

--- app/controllers/accreditation_categories_controller.rb
+++ app/controllers/accreditation_categories_controller.rb
@@ -1,5 +1,6 @@
 class AccreditationCategoriesController < ApplicationController
   before_action :set_accreditation_category, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   respond_to :html
 
