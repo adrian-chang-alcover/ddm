17735f617e22ff5bc26ee8af95a63cbc543618e8
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-05-13 22:00:28 -0400

    $ -> 	$('div#repo a.directory').click -> 		$(this).next().slideToggle() 		return false

--- app/helpers/repositorio_helper.rb
+++ app/helpers/repositorio_helper.rb
@@ -1,16 +1,20 @@
 module RepositorioHelper
   def bfs_walk(directory, level=0)
-    html = "<h#{3+level} style=\"margin-left: #{25*level}px;\">#{directory.name}</h#{3+level}>"
-    html += "<ul style=\"margin-left: #{25*level}px;\">"
+    html = "<a class=\"directory\" href=\"#\">"
+    html += "<h#{2+level}>#{directory.name}</h#{2+level}>"
+    html += "</a>"
+    html += "<div>"
+    html += "<ul data-level=\"#{level}\">"
     directory.files.sort.each do |file|
       html += "<li>"
       html += link_to file, File.join(directory.path.gsub(Rails.root.join('public').to_s,''), file)
       html += "</li>"
     end
-    html += "</ul style=\"margin-left: #{25*level}px;\">"
+    html += "</ul>"
     directory.directories.sort.each do |dir|
       html += bfs_walk(Dir.new(File.join(directory, dir)), level + 1)
     end
+    html += "</div>"
     html
   end
 end
