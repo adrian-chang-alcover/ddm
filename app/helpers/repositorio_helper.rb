3665de0e0bfb18a619e98afbcac9cb8d76b8b5bd
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-06-22 16:26:25 -0400

    html += " <span class=\"badge\">nuevo</span>" if File.stat(directory).mtime > 5.days.ago html += " <span class=\"badge\">nuevo</span>" if File.stat(File.join(directory, file)).mtime > 5.days.ago

--- app/helpers/repositorio_helper.rb
+++ app/helpers/repositorio_helper.rb
@@ -1,13 +1,18 @@
 module RepositorioHelper
   def bfs_walk(directory, level=0)
     html = "<a class=\"directory\" href=\"#\">"
-    html += "<h#{2+level}>#{directory.name}</h#{2+level}>"
+    html += "<h#{2+level}>#{directory.name}"
+    html += " <span class=\"badge\">nuevo</span>" if File.stat(directory).mtime > 5.days.ago
+    html += "</h#{2+level}>"
     html += "</a>"
     html += "<div>"
     html += "<ul data-level=\"#{level}\">"
     directory.files.sort.each do |file|
+      file_path = File.join(directory.path.gsub(Rails.root.join('public').to_s,''), file)
       html += "<li>"
-      html += link_to file, File.join(directory.path.gsub(Rails.root.join('public').to_s,''), file)
+      html += "<a href=\"#{file_path}\">#{file}"
+      html += " <span class=\"badge\">nuevo</span>" if File.stat(File.join(directory, file)).mtime > 5.days.ago
+      html += "</a>"
       html += "</li>"
     end
     html += "</ul>"
