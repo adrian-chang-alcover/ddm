d657122b9e5e5224a19c09bdfdd441d1d7837356
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-10-11 13:16:55 -0400

    showing requested tasks

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
 	skip_authorization_check	
   def index
+  	@requested_tasks = initialize_grid(TasksManagement::Task.where(requester: current_user))
   end
 end
