f8ea9d915978b11305e4f3faffdf67ea5aa4e679
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-11-11 11:31:17 -0500

    adding app/assets/javascripts/bootstrap/bootstrap-datepicker.js improving dashboard tasks

--- config/initializers/wice_grid_config.rb
+++ config/initializers/wice_grid_config.rb
@@ -124,7 +124,7 @@ if defined?(Wice::Defaults)
   # * <tt>:html5</tt> - HTML5 date input field
   # * <tt>:standard</tt> - standard Rails date and datetime helpers
   # * <tt>:bootstrap</tt> - Bootstrap datepicker helper
-  Wice::Defaults::HELPER_STYLE = :calendar
+  Wice::Defaults::HELPER_STYLE = :bootstrap
 
   # Format of the datetime displayed.
   # If you change the format, make sure to check if +DATETIME_PARSER+ can still parse this string.
