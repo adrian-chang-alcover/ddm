eff23a938cfdd93238b9cbb7021fac5df768a10d
Adrian Chang <adrian.chang.alcover@gmail.com>
2016-04-11 16:20:47 -0400

    improving Audit index and show

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,26 +1,15 @@
 class AuditsController < ApplicationController
-  before_action :set_audit, only: [:show, :edit, :update, :destroy]
   skip_authorization_check
 
   # GET /audits
   # GET /audits.json
   def index
-    @audits = initialize_grid(PaperTrail::Version)
+    @audits = initialize_grid(PaperTrail::Version.order('id DESC'))
   end
 
   # GET /audits/1
   # GET /audits/1.json
   def show
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_audit
-      @audit = PaperTrail::Version.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def audit_params
-      params.require(:audit).permit(:item_type, :item_id, :event, :whodunnit, :object)
-    end
+    @item = params[:item_type].constantize.find(params[:item_id])
+  end    
 end
